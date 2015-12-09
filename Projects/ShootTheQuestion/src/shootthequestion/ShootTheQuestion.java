8e23560f17e8d395bc9d07f99a9bf52dae98a1cf
Patricky95 <lordsquisherton@gmail.com>
2015-12-08 19:27:07 -0500

    Initial Commit

---
+++ Projects/ShootTheQuestion/src/shootthequestion/ShootTheQuestion.java
@@ -0,0 +1,14 @@
+package shootthequestion;
+
+import processing.core.PApplet;
+
+
+public class ShootTheQuestion extends PApplet {
+	private static final long serialVersionUID = 1L;
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+}
